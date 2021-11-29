0ea583c1aa666736a4c0ae346f9f4e7cb23fa9bc
qazwsx08ghj <qaq2016712@gmail.com>
2021-11-30 03:35:48 +0800

    finished Order(simple testing)

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import OrderDetail, ShoppingCart, OrderGoods
 # Register your models here.
+
+admin.site.register(OrderDetail)
+admin.site.register(ShoppingCart)
+admin.site.register(OrderGoods)
